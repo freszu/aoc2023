9875bab24cf65c30363337839d5391159f989c88
Rafal Naniewicz <rafal.naniewicz@gmail.com>
2023-12-01 20:08:03 +0100

    Solve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,33 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = input
+        .map { line ->
+            val digits = line.filter { it.isDigit() }
+            buildString { append(digits.first()); append(digits.last()) }
+        }
+        .sumOf { it.toInt() }
+
+    val possibleSpelledDigits = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    val digitsValueLookupTable: Map<String, Int> =
+        possibleSpelledDigits.mapIndexed { index, spelledDigit -> spelledDigit to (index + 1) }.toMap() +
+                (0..9).associateBy { it.toString() }
+
+    fun part2(input: List<String>): Int = input.map { line ->
+        val (_, firstDigit) = requireNotNull(line.findAnyOf(digitsValueLookupTable.keys))
+        val (_, lastDigit) = requireNotNull(line.findLastAnyOf(digitsValueLookupTable.keys))
+
+        buildString {
+            append(digitsValueLookupTable[firstDigit])
+            append(digitsValueLookupTable[lastDigit])
+        }
+    }.sumOf { it.toInt() }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput2 = readInput("Day01_test2")
+
+    check(part1(testInput) == 142)
+    check(part2(testInput2) == 281 + 18)
 
     val input = readInput("Day01")
     part1(input).println()
--- src/Utils.kt
+++ src/Utils.kt
@@ -22,3 +22,8 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun <T:Any?> T.alsoPrintln(): T {
+    println(this)
+    return this
+}
